ceb86e1a962825163630d824a78fa9cf79a98f83
Segonquart Studio {public repositories} <delfiramirez@users.noreply.github.com>
2023-12-26 18:33:49 +0100

    Update scripts.php
    Known security vulnerabilities detected
    
    Signed-off-by: Segonquart Studio {public repositories} <delfiramirez@users.noreply.github.com>

--- web/src/inc/scripts.php
+++ web/src/inc/scripts.php
@@ -25,6 +25,7 @@
 
  <![endif]-->
 <script type ="text/javascript" src ="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
 <script>
     var Twoqt_startp = (new Date()).getTime();
